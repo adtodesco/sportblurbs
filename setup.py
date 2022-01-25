a6901f8bdd5f206d2c690d9cb517b320c94c51e9
Anthony Todesco <adtodesco@gmail.com>
2022-01-24 21:46:33 -0800

    Adds setup.py; removes .py extension from script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="sportblurbs",
+    version="1.0",
+    description="sportblurbs base package",
+    author="Anthony Todesco",
+    author_email="adtodesco@gmail.com",
+    packages=["sportblurbs"],
+    scripts=["bin/process"],
+)
